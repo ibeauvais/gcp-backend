e3b233393b3401e74b4ff3e0ec06e1e3d3473dbf
ivan <ibeauvais@xebia.fr>
2019-05-16 09:39:16 +0200

    log request

--- computeEngine-hello/app.js
+++ computeEngine-hello/app.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const app = express()
 
+const morgan = require('morgan')
+
+app.use(morgan('tiny'));
+
 app.get('/', function (req, res) {
     res.send('Hello World!')
-})
+});
 
 app.get('/hello', function (req, res) {
     res.send('Hello ' + req.query.who + " !")
-})
+});
 
-app.listen(3000, function () {
-    console.log('listening on port 3000!')
-})
+
+var server = app.listen(process.env.PORT || 3000, function () {
+    console.log('Listening on port ' + server.address().port);
+});
